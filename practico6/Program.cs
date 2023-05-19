df9c18e6aea0222f0cbcf1cbe7477abd06b5b45a
julian <jnsoretniuq1998@gmail.com>
2023-05-18 21:54:03 -0300

    punto 2 terminado

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -11,7 +11,7 @@ Console.WriteLine("valor de b:"+b);*/
 texto ingresado es de hecho un número y, en caso afirmativo, realice la inversión del
 número sólo si éste es mayor a 0.*/
 
-int intNum = 0;
+/*int intNum = 0;
 bool anda= false;
 string? Numtexto = " ";
 int digito;
@@ -26,7 +26,6 @@ while (!anda)
     {
         Console.WriteLine(Numtexto + "No es un numero valido");
     }
-    
 }
 
 if(intNum > 0 )
@@ -38,4 +37,51 @@ if(intNum > 0 )
         NumReversa = NumReversa * 10 + digito;
     }
     Console.WriteLine("Número invertido: " + NumReversa);
-}
+}*/
+
+// -------------- Ejercicio 2-----------------------
+string? numeroText;
+int eleccion=1;
+float numero1,numero2, resultado=0;
+int decision;
+
+Console.WriteLine("ingrese un numero para operar");
+numeroText= Console.ReadLine();
+float.TryParse(numeroText, out numero1);
+Console.WriteLine("ingrese otro numero para operar");
+numeroText= Console.ReadLine();
+float.TryParse(numeroText, out numero2);
+
+do
+{
+    Console.WriteLine("( SELECCIONE LA OPERACION)");
+    Console.WriteLine("(1)Suma, (2)Resta, (3)Multiplicacion, (4)division");
+    numeroText= Console.ReadLine();
+    int.TryParse(numeroText, out eleccion);
+
+    switch (eleccion)
+    {
+        case 1:
+           resultado= numero1+numero2;
+        break;
+        case 2:
+           resultado= numero1-numero2;
+        break;
+        case 3:
+           resultado = numero1 * numero2;
+        break;
+        case 4:
+           if (numero2!=0)
+           {
+               resultado= numero1/numero2;
+           } else {
+               Console.WriteLine("Error: intentas dividir un numero en cero");
+           }
+        break;
+    }
+    Console.WriteLine("Resultado = "+ resultado);
+    Console.WriteLine("desea hacer otra operacion?");
+    numeroText= Console.ReadLine();
+    int.TryParse(numeroText, out decision);
+
+} while (decision==1);
