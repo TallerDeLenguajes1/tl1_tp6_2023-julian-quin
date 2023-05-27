48d17f05208a08df729671233da1c2ccf1db7ffc
julian <jnsoretniuq1998@gmail.com>
2023-05-27 18:41:22 -0300

    punto 3 completo

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -61,53 +61,36 @@ ya que no se puede realizar ninguna conversión.
 
 // -------------- Ejercicio 2-----------------------
 string? numeroText;
-int eleccion=1;
 float numero1=0,numero2=0, resultado=0;
 int opcion;
 bool valido= false;
+float max,min;
 do
 {
     Console.WriteLine("( SELECCIONE LA OPERACION)");
-    Console.WriteLine(" \t(1)Suma\n \t(2)Resta\n \t(3)Multiplicacion\n \t(4)division\n");
+    Console.WriteLine(" \t(1) Suma\n \t(2) Resta\n \t(3) Multiplicacion\n \t(4) division\n \t(5) Op. avanzadas a un numero\n \t(6) Max y Min");
     numeroText= Console.ReadLine();
     int.TryParse(numeroText, out opcion);
-    while (!valido)
-    {
-        Console.WriteLine("Dame el 1er numero: ");
-        numeroText= Console.ReadLine();
-        valido=float.TryParse(numeroText, out numero1);
-        if (!valido)
-        {
-            Console.WriteLine("El numero " + numeroText + " no es valido");
-        }
-    }
-
-    valido=false;
-
-    while (!valido)
-    {
-        Console.WriteLine("Dame el 2do numero: ");
-        numeroText= Console.ReadLine();
-        valido=float.TryParse(numeroText, out numero2);
-        if (!valido)
-        {
-            Console.WriteLine("El numero " + numeroText + " no es valido");
-        }
-    }
-
-    valido = false;
-    switch (eleccion)
+    switch (opcion)
     {
         case 1:
+           numero1=validarNum();
+           numero2=validarNum();
            resultado= numero1+numero2;
         break;
         case 2:
+            numero1=validarNum();
+            numero2=validarNum();
            resultado= numero1-numero2;
         break;
         case 3:
+            numero1=validarNum();
+           numero2=validarNum();
            resultado = numero1 * numero2;
         break;
         case 4:
+           numero1=validarNum();
+           numero2=validarNum();
            if (numero2!=0)
            {
                resultado= numero1/numero2;
@@ -115,10 +98,45 @@ do
                Console.WriteLine("Error: intentas dividir un numero en cero");
            }
         break;
+        case 5:
+           numero1=validarNum();
+           Console.WriteLine("Valor absoluto: "+ Math.Abs(numero1));
+           Console.WriteLine("Cuadrado: "+ Math.Pow(numero1,2));
+           if(numero1 >= 0) Console.WriteLine("Raiz Cuadrada: "+ Math.Sqrt(numero1));
+           else Console.WriteLine("Raiz Cuadrada: no existe en los R");
+           Console.WriteLine("Seno: "+ Math.Sin(numero1));
+           Console.WriteLine("coseno: "+ Math.Cos(numero1));
+        break;
+        case 6:
+           numero1=validarNum();
+           numero2=validarNum();
+           max=Math.Max(numero1, numero2);
+           min=Math.Min(numero1, numero2);
+           Console.WriteLine("Maximo: "+ max);
+           Console.WriteLine("Minimo: "+ min);
+        break; 
     }
-    Console.WriteLine("Resultado = "+ resultado);
-    Console.WriteLine("¿ Desea hacer otra operacion ?");
+    if (opcion >= 1 && opcion <= 5) Console.WriteLine("Resultado = "+ resultado);
+    Console.WriteLine("¿ Desea hacer otra operacion 1=Ok o 0=No ?");
     numeroText= Console.ReadLine();
     int.TryParse(numeroText, out opcion);
-
+ 
 } while (opcion==1);
+
+
+float validarNum ()
+{
+    float num=0;  // solo la ve la funcion
+    while (!valido)
+    {
+        Console.WriteLine("Dame un numero: ");
+        numeroText= Console.ReadLine();
+        valido=float.TryParse(numeroText, out num);
+        if (!valido)
+        {
+            Console.WriteLine("El numero " + numeroText + " no es valido");
+        }
+    }
+    valido=false;
+    return num;
+}
